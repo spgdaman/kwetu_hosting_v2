10571be52b1bb17426168c6e6f3dc10ee0505738
simon <gatheru.simon432@gmail.com>
2019-06-19 17:36:54 +0300

    file upload view created

--- api/views.py
+++ api/views.py
@@ -19,3 +19,15 @@ class ListAssets(generics.ListCreateAPIView):
 class DetailAssets(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Assets.objects.all()
     serializer_class = serializers.AssetSerializer
+
+class FileUploadView(APIView):
+    parser_class = (FileUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+        asset_serializer = serializers.FileSerializer(data=request.data)
+
+        if asset_serializer.is_valid():
+            asset_serializer.save()
+            return Response(asset_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(asset_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
