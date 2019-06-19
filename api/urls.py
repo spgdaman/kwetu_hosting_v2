68789bd318ab483b3f562adc8f011ded9175ff1a
simon <gatheru.simon432@gmail.com>
2019-06-19 17:47:17 +0300

    url patterns added to api urls file

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.ListAssets.as_view()),
+    path('asset_id/<int:pk>/', views.DetailAssets.as_view()),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', incude('rest_auth.registration.urls')),
+    path('upload_video/', views.FileUploadView.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
