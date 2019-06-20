311f57e6f432beb3743fbe76f9a49c9ccd81370d
simon <gatheru.simon432@gmail.com>
2019-06-20 08:54:10 +0300

    commit before push

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)       
+class Profile(models.Model):      
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     bio = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profile/')
+    profile_pic = models.FileField(upload_to='profile/')
     pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    # Foreign Key
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
 
     def __str__(self):
         return self.first_name
@@ -24,7 +26,13 @@ class Profile(models.Model):
         return profiles
 
 class Assets(models.Model):
-    image = models.ImageField(upload_to='media/')
+    name = models.CharField(max_length=60)
+    assetfile = models.FileField(upload_to='media/')
     date_time = models.DateTimeField(auto_now_add=True)
+
+    # Foreign Key
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.date_time
 
